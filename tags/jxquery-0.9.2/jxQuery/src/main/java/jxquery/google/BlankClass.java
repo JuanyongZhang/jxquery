20db0babd0ab8ca39009354e9e6bc7b6f42aec38
Juanyong.zhang@gmail.com <Juanyong.zhang@gmail.com@59d4f8a0-56e6-1feb-02fa-26c4f8f9b2cb>
2011-11-14 18:20:12 +0000

    [maven-release-plugin]  copy for tag jxquery-0.9.2

---
+++ tags/jxquery-0.9.2/jxQuery/src/main/java/jxquery/google/BlankClass.java
@@ -0,0 +1,10 @@
+package jxquery.google;
+
+/**
+ * @author <a href="mailto:Juanyong.zhang@gmail.com">Juanyong Zhang</a>
+ * @date Nov 11, 2011
+ */
+public final class BlankClass {
+	private BlankClass() {
+	}
+}
---
+++ tags/jxquery-0.9.2/jxQuery/src/main/java/jxquery/google/creator/FieldCreator.java
@@ -0,0 +1,18 @@
+package jxquery.google.creator;
+
+/**
+ * Create a custom field value on the input object.
+ * <p>
+ * Object is {@link org.w3c.dom.Node Node} or {@link org.w3c.dom.NodeList
+ * NodeList}
+ * </p>
+ * 
+ * @Author:<a href="mailto:Juanyong.zhang@carefirst.com">Cham Juanyong.zhang</a>
+ * @Date:Sep 29, 2011
+ * 
+ */
+public interface FieldCreator {
+
+	public Object create(Object org);
+
+}

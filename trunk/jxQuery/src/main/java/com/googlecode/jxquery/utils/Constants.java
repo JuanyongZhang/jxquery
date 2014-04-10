62f1cd2d4ada8d9ec88b242320b895a82c4d2bd4
Juanyong.zhang@gmail.com <Juanyong.zhang@gmail.com@59d4f8a0-56e6-1feb-02fa-26c4f8f9b2cb>
2014-04-10 18:31:25 +0000

    updated the groupId and package

---
+++ trunk/jxQuery/src/main/java/com/googlecode/jxquery/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.googlecode.jxquery.utils;
+
+/**
+ * @Author : <a href="mailto:Juanyong.zhang@gmail.com">Juanyong.zhang</a>
+ * @Date : Sep 25, 2011
+ */
+public interface Constants {
+	public static final String XML_ROOT = "xmlRoot";
+
+	public static final String XML_EMBEDDED_JSON = "_embedded_json";
+
+	public static final String SLASH = "/";
+
+	public static final String JSON_ARRAY = "JSONArray";
+}
---
+++ trunk/jxQuery/src/test/java/com/googlecode/jxquery/test/person/xml/PhoneNumber.java
@@ -0,0 +1,30 @@
+package com.googlecode.jxquery.test.person.xml;
+
+import com.googlecode.jxquery.jxQuery;
+
+/**
+ * @Author : <a href="mailto:Juanyong.zhang@gmail.com">Juanyong.zhang</a>
+ * @Date : Aug 31, 2011
+ */
+public class PhoneNumber {
+	@jxQuery(query = "phoneNumber/type")
+	private String type;
+	@jxQuery(query = "phoneNumber/number")
+	private String number;
+
+	public String getNumber() {
+		return number;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
